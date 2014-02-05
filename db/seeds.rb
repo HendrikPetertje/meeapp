65feae221374dfe0c2701349833474118070c87b
johman10 <johan@MacBook-Pro-van-Johan.local>
2014-02-05 18:06:34 +0100

    Updated seed for content support

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,46 @@ pages = Page.create([
 { title: 'Home', content: '<p><strong>Wat doen we?</strong><br>
 MEE ondersteunt mensen met een beperking.&nbsp;U kunt bij MEE terecht met uw vragen over opvoeding &amp; ontwikkeling, leren &amp; werken, samenleven &amp; wonen en regelgeving &amp; geldzaken.&nbsp;U wilt zo zelfstandig en onafhankelijk mogelijk meedoen in de samenleving. En zelf de regie houden over uw leven. Wij kennen de mogelijkheden en helpen u hier graag bij. Een indicatie of verwijsbrief is niet nodig. Onze ondersteuning is gratis. U vindt ons door het hele land.&nbsp;Bekijk hier een filmpje waarin mensen met een beperking laten zien wat meedoen voor ze betekent.</p>' },
 { title: 'Contact', content: 'Dit is de inhoud van deze pagina' },
-{ title: 'FAQ', content: 'Dit is de inhoud van deze pagina' },
+{ title: 'FAQ', content: "Voor vragen over opvoeding en ontwikkeling voor kinderen, jongeren, hun ouders en andere betrokkenen
+
+'Ons dochtertje ontwikkelt zich anders dan onze andere kinderen. Wat is er met haar aan de hand?'
+'Hoe ga ik om met mijn kind in de puberteit?'
+'Mijn broer is autistisch, hoe ga ik met hem om?'
+'Hoe ga ik om met de beperking van mijn broertje of zusje?
+'Welke dagopvang is geschikt voor ons kind?'
+​Acceptatie
+Hoe accepteert u uw beperking of de beperking van uw kind? Hoe gaat u ermee om? En hoe versterkt u uw vaardigheden of die van uw kind, zodat u of uw kind zo zelfstandig mogelijk kunt zijn?
+
+Ondersteuning
+We ondersteunen ook (groot)ouders, familie en vrienden bij het accepteren van uw beperking of de beperking van uw kind. Heeft u niet zoveel mensen in de buurt waarop u kunt terugvallen? Dan helpen wij u bij het vinden van betrouwbare mensen die u kunnen helpen. Soms is er professionele zorg nodig. Samen onderzoeken we de mogelijkheden.
+
+Voor vragen over leren en werken
+
+‘Wat is de beste school voor ons kind?’
+‘Kan mijn kind een betaalde baan aan? Of is een beschermde werkplek of dagbesteding het hoogst haalbare?’
+'Hoe vind ik werk dat bij mij past?'
+Welke risico’s loop ik als werkgever als ik iemand met een beperking in dienst neem?
+School
+Bent u op zoek naar een geschikte school of kinderopvang voor uw kind? Samen met u kijken we wat uw wensen zijn. En wat er mogelijk is voor uw kind. Heeft u een keuze gemaakt? Dan helpen we u als dat nodig is bij de plaatsing op de school of de kinderopvang. Ook regelen wij de begeleiding of zorg die nodig is. En we ondersteunen de opvang of school – bijvoorbeeld door informatie te geven over de beperking van uw kind. 
+
+Werk 
+Werk vinden is niet altijd makkelijk. Zeker niet met een beperking. Maar u hoeft het niet alleen te doen. Samen met u en uw familie, vrienden of kennissen kijken we naar de mogelijkheden. Een stagetraject, een betaalde baan, een beschutte werkplek of dagbesteding. We kijken samen wat het beste bij u past. Ook helpen we bij de overgang van school naar werk, als dat nodig is. We regelen de begeleiding of zorg die nodig is. Als u moeilijkheden tegenkomt op het werk, zoeken we naar een oplossing. En we ondersteunen uw werkgever en collega’s – bijvoorbeeld door informatie te geven over uw beperking.
+
+Voor ondersteuning en advies bij wonen en contact met anderen
+
+'Hoe ga ik om met de beperking van mijn partner?'
+'Welke woonvorm past het beste bij mijn zoon?'
+‘Ik zit vaak achter de computer, maar ik heb liever vrienden om me heen. Hoe pak ik dat aan?’
+Contacten leggen
+Niet iedereen maakt even gemakkelijk contact. Hoe kunt u een vrienden- en kennissenkring opbouwen die u kunt vertrouwen? Samen met u, uw familie en vrienden onderzoeken we uw wensen. We leren u contacten te leggen door bijvoorbeeld een cursus Sociale Vaardigheden te volgen. En we ondersteunen uw nieuwe vrienden en kennissen. Bijvoorbeeld met informatie over uw beperking en hoe ze daarmee om kunnen gaan.
+
+Wonen
+Wonen met een beperking, hoe doet u dat? Samen met u, uw familie, vrienden of kennissen zetten we op een rij wat de mogelijkheden zijn. Ook helpen we bij het vinden en regelen van een woonvorm die bij u past. Compleet met voorzieningen en zorg, als dat nodig is. Soms is het nodig om te leren hoe u een huishouden voert. En om zonder problemen en overlast te kunnen wonen. Wij helpen u hierbij ondersteuning te vinden.  Als u niet al te lang hulp nodig heeft, dan kunnen wij dat ook zelf. Bijvoorbeeld met behulp van een training.   
+
+Vrienden en relaties
+Hoe vind ik vrienden? Hoe ga ik een relatie aan? Samen met u onderzoeken we uw wensen. Niet iedereen vindt het gemakkelijk om een praatje met anderen te maken. Soms weet u niet goed hoe u dit moet aanpakken. Als dat nodig is, helpen wij u met bijvoorbeeld het aanleren van sociale vaardigheden. Daar geven wij trainingen voor. Ook zijn er verschillende vriendenkringen en maatjesprojecten die het makkelijker maken vriendschappen te sluiten. 
+
+Gezondheid
+Wilt u sporten om zich fitter te voelen? Of overgewicht voorkomen of tegengaan? Als u gezonder wilt leven, maar u weet niet goed hoe dat moet, dan onderzoeken we samen met u en uw familie, vrienden en kennissen hoe u geholpen kunt worden. Bijvoorbeeld met advies. Of een doorverwijzing naar de huisarts of een diëtist. Ook geven wij informatie over sport- en vrijetijdsvoorzieningen." },
 { title: 'Voorwaarden', content: 'Dit is de inhoud van deze pagina' }
 ])
