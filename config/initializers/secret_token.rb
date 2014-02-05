8623d1ee59b481fe905b23c6cddc8b4edae4ea12
Peter van der Meulen <peter@picoreclame.nl>
2014-02-05 10:41:09 +0100

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meeapp::Application.config.secret_key_base = 'fc1b16dcd96bf42d68ec332444610051909c46c9a2231418003dca02d05bf88989924d4d03dac22be0e6972aa03bbf2de7bc25eda567f44c8e35c0fe7c49bb61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meeapp::Application.config.session_store :cookie_store, key: '_meeapp_session'
